cd4e9a9540ca53b392a83a304bf5453d4d342826
Akasha Sahu <akashasahu2001@gmail.com>
2025-03-10 22:33:34 +0530

    update js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let wrapper = document.getElementsByClassName("wrapper")[0];
 let template = document.getElementsByTagName("template")[0];
 
-let names = ["Peter", "Tony", "Steve"]
+let names = ["Peter", "Tony", "Steve"];
 
 let colors = [
     "#FF5733", "#33FF57", "#3357FF", "#FF33A1", "#A133FF",
@@ -12,23 +12,38 @@ let colors = [
     "#CD5C5C", "#BA55D3", "#3CB371", "#FF6347", "#40E0D0"
 ];
 
-let sticker = function (name) {
-    let div = template.content.querySelector("div");
-    let nameOfStick = div.querySelector(".name");
+// Store references to stickers
+let stickers = [];
 
+let createSticker = function (name) {
+    let div = template.content.querySelector("div").cloneNode(true);
+    let nameOfStick = div.querySelector(".name");
     nameOfStick.innerHTML = name;
 
-    div.style.background = colors[Math.floor(Math.random() * colors.length)];
+    let randomColor = colors[Math.floor(Math.random() * colors.length)];
+    div.style.background = randomColor;
+    let initialRotation = Math.random() * 40 - 20;
+    div.style.transform = `rotate(${initialRotation}deg)`;
 
-    div.style.transform = "rotate(" + (Math.random() * 40 - 20) + "deg)";
+    wrapper.appendChild(div);
+    stickers.push({ element: div, rotation: initialRotation });
+};
 
-    let node = document.importNode(div, true);
-    wrapper.appendChild(node);
+// Create stickers
+names.forEach(createSticker);
+
+// Function to continuously rotate stickers and change colors even faster
+function animateStickers() {
+    stickers.forEach(sticker => {
+        sticker.rotation += (Math.random() * 10 - 5); // Even faster rotation
+        sticker.element.style.transform = `rotate(${sticker.rotation}deg)`;
+
+        // Assign a new random color to each sticker
+        let newColor = colors[Math.floor(Math.random() * colors.length)];
+        sticker.element.style.background = newColor;
+    });
+
+    setTimeout(animateStickers, 1000); // Faster updates every 300ms
 }
 
-setTimeout(refreshPage, 1000);
-function refreshPage() {
-    location.reload();
-}
-
-names.forEach(sticker);
+animateStickers();
